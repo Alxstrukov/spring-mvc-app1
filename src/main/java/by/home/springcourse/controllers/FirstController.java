99fc95985b551e527ec0f47e0b8282cfd3010809
Alexandr Strukov <alxstrukov@yandex.by>
2023-09-29 13:40:52 +0300

    add classes Person, PersonDAO, PeopleController

--- src/main/java/by/home/springcourse/controllers/FirstController.java
+++ src/main/java/by/home/springcourse/controllers/FirstController.java
@@ -1,19 +1,67 @@
 package by.home.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
+    /*@GetMapping("/hello")
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        System.out.println("Hello, " + name + " " + surname);
+        return "first/hello";
+    }*/
+
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
 
+
     @GetMapping("/goodbye")
-    public String goodByePage(){
+    public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a") int a,
+                             @RequestParam(value = "b") int b,
+                             @RequestParam(value = "action") String action,
+                             Model model) {
+
+        double result;
+        switch (action) {
+            case "multiplication": {
+                result = a * b;
+            }
+            break;
+            case "addition": {
+                result = a + b;
+            }
+            break;
+            case "subtraction": {
+                result = a - b;
+            }
+            break;
+            case "division": {
+                result = a / (double) b;
+            }
+            break;
+            default: {
+                result = 0;
+            }
+            break;
+        }
+        model.addAttribute("result", "Result = " + result);
+        return "first/calculator";
     }
 }
---
+++ src/main/java/by/home/springcourse/dao/PersonDAO.java
@@ -0,0 +1,29 @@
+package by.home.springcourse.dao;
+
+import by.home.springcourse.models.Person;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    public static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
